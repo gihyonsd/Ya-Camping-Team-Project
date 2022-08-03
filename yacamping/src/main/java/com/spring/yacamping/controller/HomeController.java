f56a7559ce3d7e4f3fa7af8a2fd71d1fb526a5c2
Noah <gihyonsd@gmail.com>
2022-08-03 18:15:30 +0900

    스프링 - home에서 db값 받아오기 완료

--- yacamping/src/main/java/com/spring/yacamping/controller/HomeController.java
+++ yacamping/src/main/java/com/spring/yacamping/controller/HomeController.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.spring.yacamping.service.BoardService;
 
 @Controller
 public class HomeController {
+	@Autowired 
 	private BoardService service;
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
