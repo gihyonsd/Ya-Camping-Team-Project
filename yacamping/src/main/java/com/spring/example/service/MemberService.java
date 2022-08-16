b1ebec172d3003182261b3b7be6f0b4b4d64f71b
gihyonsd <48745104+gihyonsd@users.noreply.github.com>
2022-08-16 10:17:55 +0900

    Delete yacamping directory

--- yacamping/src/main/java/com/spring/example/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.spring.example.service;
-
-import com.spring.example.VO.MemberVO;
-
-public interface MemberService {
-    public int Login(MemberVO vo) throws Exception;
-    
-    public void register(MemberVO vo) throws Exception;
-}
--- yacamping/src/main/webapp/resources/js/complete.js
+++
@@ -1,8 +0,0 @@
-/* 3초 후 자동으로 메인으로 이동 */
-window.onload = function () { 
-  setTimeout("startPage()", 3000
-  ); 
-}
-function startPage() { 
-  document.location.href = "/"; 
-}
